10e4c41e3acdc7f841d779356ff06dd58170de97
GitHub Action <action@github.com>
2023-07-10 07:56:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/chintohere/intellijopenfileinremoteplugin/MyBundle.kt
+++ src/main/kotlin/com/github/chintohere/intellijopenfileinremoteplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.chintohere.intellijopenfileinremoteplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/chintohere/intellijopenfileinremoteplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chintohere/intellijopenfileinremoteplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.chintohere.intellijopenfileinremoteplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chintohere.intellijopenfileinremoteplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
